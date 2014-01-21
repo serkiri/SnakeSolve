dfa0967ae1310ef434428bc60b7ba525109c92fb
Sergiy Kyrylenko <sergiy.kyrylenko@travisperkins.co.uk>
2014-01-21 18:07:08 +0200

    Create SnakeSolver

---
+++ src/com/snake/piece/PieceType.java
@@ -0,0 +1,5 @@
+package com.snake.piece;
+
+public enum PieceType {
+	TERMINATED_PIECE, STRAIGHT_PIECE, BENT_PIECE
+}
---
+++ src/com/snake/solver/SnakeSolver.java
@@ -0,0 +1,46 @@
+package com.snake.solver;
+
+import com.snake.piece.PieceType;
+
+import static com.snake.piece.PieceType.*;
+
+
+public class SnakeSolver {
+	
+	
+	
+	private static final PieceType[] SNAKE = {
+		TERMINATED_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		BENT_PIECE,
+		STRAIGHT_PIECE,
+		TERMINATED_PIECE
+	};
+	
+	public static void main(String[] args) {
+
+	}
+
+}
