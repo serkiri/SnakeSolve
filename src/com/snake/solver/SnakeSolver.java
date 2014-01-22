ed3658bff6dfe2e55ca206ab8147ad60c10f6463
Sergiy Kyrylenko <serkiri@list.ru>
2014-01-22 23:34:26 +0200

    Brake project

--- src/com/snake/solver/SnakeSolver.java
+++ src/com/snake/solver/SnakeSolver.java
@@ -1,13 +1,15 @@
 package com.snake.solver;
 
+import java.util.ArrayList;
+
+import com.snake.piece.AbstractPiece;
 import com.snake.piece.PieceType;
+import com.snake.piece.TerminatedPiece;
 
 import static com.snake.piece.PieceType.*;
 
 
 public class SnakeSolver {
-	
-	
 	
 	private static final PieceType[] SNAKE = {
 		TERMINATED_PIECE,
@@ -39,8 +41,35 @@ public class SnakeSolver {
 		TERMINATED_PIECE
 	};
 	
-	public static void main(String[] args) {
+	private ArrayList<AbstractPiece> snake;
+	private int placedPieceNum;
 
+	public void prepareNewSnake(){
+		snake = new ArrayList<AbstractPiece>();
+		snake.add(new TerminatedPiece());
+		placedPieceNum = 0;
+	}
+
+	private void placeNextPiece() {
+		if()snake.get
+	}
+
+	private void solve() {
+//		while(true){
+		placeNextPiece();	
+//		}
+	}
+
+	public void printSnake(){
+		System.out.println(snake);
+	}
+	
+	public static void main(String[] args) {
+		SnakeSolver solver = new SnakeSolver();
+		solver.prepareNewSnake();
+		solver.printSnake();
+		solver.solve();
+		
 	}
 
 }
